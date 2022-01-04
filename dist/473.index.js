b2fb12be6846178d25fce735f25549fbb5efa7ef
danadajian <danadajian@gmail.com>
2022-01-04 11:07:33 -0600

    feat(merge-queue): update first queued pr with default branch (#51)

--- dist/473.index.js
+++ dist/473.index.js
@@ -1,5 +1,5 @@
 exports.id = 473;
-exports.ids = [473,61,209];
+exports.ids = [473,4,61,209];
 exports.modules = {
 
 /***/ 9042:
@@ -11,7 +11,6 @@ exports.modules = {
 /* harmony export */   "$9": () => (/* binding */ DEFAULT_PIPELINE_STATUS),
 /* harmony export */   "Km": () => (/* binding */ DEFAULT_PIPELINE_DESCRIPTION),
 /* harmony export */   "Hc": () => (/* binding */ PRODUCTION_ENVIRONMENT),
-/* harmony export */   "mj": () => (/* binding */ DEFAULT_BRANCH),
 /* harmony export */   "_d": () => (/* binding */ CORE_APPROVED_PR_LABEL),
 /* harmony export */   "Xt": () => (/* binding */ PEER_APPROVED_PR_LABEL),
 /* harmony export */   "Ak": () => (/* binding */ READY_FOR_MERGE_PR_LABEL),
@@ -44,7 +43,6 @@ const DEFAULT_EXEMPT_DESCRIPTION = 'Passed in case the check is exempt.';
 const DEFAULT_PIPELINE_STATUS = 'Pipeline Status';
 const DEFAULT_PIPELINE_DESCRIPTION = 'Pipeline clear.';
 const PRODUCTION_ENVIRONMENT = 'production';
-const DEFAULT_BRANCH = 'main';
 const CORE_APPROVED_PR_LABEL = 'CORE APPROVED';
 const PEER_APPROVED_PR_LABEL = 'PEER APPROVED';
 const READY_FOR_MERGE_PR_LABEL = 'READY FOR MERGE';
@@ -83,6 +81,8 @@ var octokit = __webpack_require__(6161);
 var remove_label = __webpack_require__(61);
 // EXTERNAL MODULE: ./src/helpers/set-commit-status.ts
 var set_commit_status = __webpack_require__(2209);
+// EXTERNAL MODULE: ./src/helpers/prepare-queued-pr-for-merge.ts
+var prepare_queued_pr_for_merge = __webpack_require__(1004);
 ;// CONCATENATED MODULE: ./src/utils/update-merge-queue.ts
 /*
 Copyright 2021 Expedia, Inc.
@@ -111,6 +111,7 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
 
 
 
+
 const updateMergeQueue = (queuedPrs) => {
     const sortedPrs = sortPrsByQueuePosition(queuedPrs);
     return (0,bluebird.map)(sortedPrs, updateQueuePosition);
@@ -131,7 +132,7 @@ const sortPrsByQueuePosition = (queuedPrs) => queuedPrs
 const updateQueuePosition = (pr, index) => __awaiter(void 0, void 0, void 0, function* () {
     const { number, label, queuePosition } = pr;
     const newQueuePosition = index + 1;
-    if (!label || queuePosition === newQueuePosition) {
+    if (!label || isNaN(queuePosition) || queuePosition === newQueuePosition) {
         return;
     }
     if (newQueuePosition === 1) {
@@ -143,7 +144,8 @@ const updateQueuePosition = (pr, index) => __awaiter(void 0, void 0, void 0, fun
                 state: 'success',
                 description: 'This PR is next to merge.'
             }),
-            (0,remove_label.removeLabelIfExists)(constants/* JUMP_THE_QUEUE_PR_LABEL */.nJ, number)
+            (0,remove_label.removeLabelIfExists)(constants/* JUMP_THE_QUEUE_PR_LABEL */.nJ, number),
+            (0,prepare_queued_pr_for_merge.updatePrWithMainline)(pullRequest)
         ]);
     }
     return Promise.all([
@@ -221,6 +223,62 @@ const getQueuedPrData = () => {
         q: `org:${owner}+repo:${repo}+is:pr+is:open+label:"${constants/* READY_FOR_MERGE_PR_LABEL */.Ak}"`
     });
 };
+
+
+/***/ }),
+
+/***/ 1004:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "prepareQueuedPrForMerge": () => (/* binding */ prepareQueuedPrForMerge),
+/* harmony export */   "updatePrWithMainline": () => (/* binding */ updatePrWithMainline)
+/* harmony export */ });
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2186);
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _constants__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9042);
+/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5438);
+/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _octokit__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(6161);
+/*
+Copyright 2021 Expedia, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+
+
+const prepareQueuedPrForMerge = () => _octokit__WEBPACK_IMPORTED_MODULE_3__/* .octokit.pulls.list */ .K.pulls.list(Object.assign({ state: 'open', per_page: 100 }, _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.repo))
+    .then(findNextPrToMerge)
+    .then(pullRequest => {
+    if (pullRequest) {
+        return updatePrWithMainline(pullRequest);
+    }
+});
+const findNextPrToMerge = (pullRequestsResponse) => {
+    var _a;
+    return (_a = pullRequestsResponse.data.find(pr => hasRequiredLabels(pr, [_constants__WEBPACK_IMPORTED_MODULE_1__/* .READY_FOR_MERGE_PR_LABEL */ .Ak, _constants__WEBPACK_IMPORTED_MODULE_1__/* .JUMP_THE_QUEUE_PR_LABEL */ .nJ]))) !== null && _a !== void 0 ? _a : pullRequestsResponse.data.find(pr => hasRequiredLabels(pr, [_constants__WEBPACK_IMPORTED_MODULE_1__/* .READY_FOR_MERGE_PR_LABEL */ .Ak, _constants__WEBPACK_IMPORTED_MODULE_1__/* .FIRST_QUEUED_PR_LABEL */ .IH]));
+};
+const hasRequiredLabels = (pr, requiredLabels) => requiredLabels.every(mergeQueueLabel => pr.labels.some(label => label.name === mergeQueueLabel));
+const updatePrWithMainline = (pullRequest) => _octokit__WEBPACK_IMPORTED_MODULE_3__/* .octokit.repos.merge */ .K.repos.merge(Object.assign({ base: pullRequest.head.ref, head: 'HEAD' }, _actions_github__WEBPACK_IMPORTED_MODULE_2__.context.repo))
+    .catch(error => {
+    if (error.status === 204) {
+        _actions_core__WEBPACK_IMPORTED_MODULE_0__.info('The first PR in the queue is already up to date!');
+    }
+    if (error.status === 409) {
+        _actions_core__WEBPACK_IMPORTED_MODULE_0__.info('The first PR in the queue has a merge conflict.');
+    }
+});
 
 
 /***/ }),
