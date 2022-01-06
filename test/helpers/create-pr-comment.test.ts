466f9720e1f5c5726220fb06f2c38b691535a406
danadajian <danadajian@gmail.com>
2022-01-06 11:19:15 -0600

    feat(create-pr-comment): support comment overwriting (#70)

--- test/helpers/create-pr-comment.test.ts
+++ test/helpers/create-pr-comment.test.ts
@@ -11,6 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { Mocktokit } from '../types';
 import { context } from '@actions/github';
 import { createPrComment } from '../../src/helpers/create-pr-comment';
 import { octokit } from '../../src/octokit';
@@ -18,23 +19,78 @@ import { octokit } from '../../src/octokit';
 jest.mock('@actions/core');
 jest.mock('@actions/github', () => ({
   context: { repo: { repo: 'repo', owner: 'owner' }, issue: { number: 123 } },
-  getOctokit: jest.fn(() => ({ rest: { issues: { createComment: jest.fn() } } }))
+  getOctokit: jest.fn(() => ({
+    rest: {
+      issues: {
+        createComment: jest.fn(),
+        listComments: jest.fn(),
+        updateComment: jest.fn()
+      }
+    }
+  }))
+}));
+
+(octokit.issues.listComments as unknown as Mocktokit).mockImplementation(async () => ({
+  data: [
+    {
+      id: 12345,
+      body: 'body',
+      user: {
+        login: 'login'
+      }
+    },
+    {
+      id: 456,
+      body: 'some other body',
+      user: {
+        login: 'some other login'
+      }
+    }
+  ]
 }));
 
 describe('createPrComment', () => {
-  const body = 'body';
+  describe('create comment case', function () {
+    const body = 'body';
 
-  beforeEach(() => {
-    createPrComment({
-      body
+    beforeEach(() => {
+      createPrComment({ body });
+    });
+
+    it('should call createComment with correct params', () => {
+      expect(octokit.issues.createComment).toHaveBeenCalledWith({
+        body,
+        issue_number: 123,
+        ...context.repo
+      });
     });
   });
 
-  it('should call createComment with correct params', () => {
-    expect(octokit.issues.createComment).toHaveBeenCalledWith({
-      body,
-      issue_number: 123,
-      ...context.repo
+  describe('update comment case', function () {
+    const body = 'body';
+    const login = 'login';
+
+    beforeEach(() => {
+      createPrComment({ body, login });
+    });
+
+    it('should not call createComment with correct params', () => {
+      expect(octokit.issues.createComment).not.toHaveBeenCalled();
+    });
+
+    it('should call listComments with correct params', () => {
+      expect(octokit.issues.listComments).toHaveBeenCalledWith({
+        issue_number: 123,
+        ...context.repo
+      });
+    });
+
+    it('should call updateComment with correct params', () => {
+      expect(octokit.issues.updateComment).toHaveBeenCalledWith({
+        comment_id: 12345,
+        body,
+        ...context.repo
+      });
     });
   });
 });
