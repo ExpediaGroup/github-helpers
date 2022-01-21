e9e9b8b11b8ffc02e2be571b218dde1c6064f215
danadajian <dadajian@expediagroup.com>
2022-01-21 15:08:46 -0600

    chore: add file copyright headers check (#94)

---
+++ scripts/verify-file-headers.ts
@@ -0,0 +1,17 @@
+import getFiles from 'https://deno.land/x/getfiles@v1.0.0/mod.ts';
+
+const filePaths = getFiles({ root: '.', include: ['src', 'test'] }).map(file => file.path);
+const filesWithoutCopyrightHeader =
+  (await Promise.all(
+    filePaths.map(async filePath => {
+      const fileText = await Deno.readTextFile(filePath);
+      return fileText.startsWith('/*\nCopyright') ? undefined : filePath;
+    })
+  )).filter(Boolean);
+
+if (filesWithoutCopyrightHeader.length) {
+  console.error(`\nThe following files are missing a valid copyright header:${filesWithoutCopyrightHeader.map(file => `\n   • ${file}`).join()}`);
+  Deno.exit(1);
+}
+
+console.info('All files contain a valid copyright header!');
