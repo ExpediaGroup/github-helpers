aeb03ae92d3a4f0bc98ac1538a1b21cf939de0c6
danadajian <danadajian@gmail.com>
2022-01-26 20:13:01 -0600

    committing progress

--- plopfile.js
+++ plopfile.js
@@ -1,15 +1,43 @@
+// eslint-disable-next-line functional/immutable-data,import/no-commonjs
 module.exports = plop => {
-  plop.setGenerator('controller', {
-    description: 'application controller logic',
-    prompts: [{
-      type: 'input',
-      name: 'name',
-      message: 'controller name please'
-    }],
-    actions: [{
-      type: 'add',
-      path: 'src/{{name}}.js',
-      templateFile: 'plop-templates/controller.hbs'
-    }]
+  plop.setHelper('spaceSeparatedCase', helper => helper.split('-').join(' '));
+
+  plop.setGenerator('helper', {
+    description: 'new github helper',
+    prompts: [
+      {
+        type: 'input',
+        name: 'helper',
+        message: 'New helper name (e.g. my-new-helper):'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a detailed description of what the helper does and how it can be used:'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/helpers/{{ helper }}.ts',
+        templateFile: 'templates/helper.hbs'
+      },
+      {
+        type: 'add',
+        path: 'test/helpers/{{ helper }}.test.ts',
+        templateFile: 'templates/test.hbs'
+      },
+      {
+        type: 'add',
+        path: '.github/workflows/{{ helper }}.yml',
+        templateFile: 'templates/workflow.hbs'
+      },
+      {
+        type: 'append',
+        path: 'README.md',
+        pattern: /Each of the following helpers are defined in a file of the same name in `src\/helpers`:\n/g,
+        templateFile: 'templates/docs.hbs'
+      }
+    ]
   });
 };
