47abb4631160bc5932a3eb0fe2e44f721424e8d1
DESKTOP-A9MHFN4\PL-WORK <regisztralolaszlo@gmail.com>
2022-01-28 13:05:35 +0100

    test commit 001

--- src/helpers/set-deployment-status.ts
+++ src/helpers/set-deployment-status.ts
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Test change 001
+
 import { DeploymentState } from '../types';
 import { GITHUB_OPTIONS } from '../constants';
 import { context } from '@actions/github';
