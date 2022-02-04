65f3a4dab73816b0b24204f47c641df5c78f8aa4
Andrew Schwenn <aschwenn@verizon.net>
2022-02-04 14:17:38 -0600

    Update src/helpers/set-commit-status.ts
    Co-authored-by: Dan Adajian <dadajian@expediagroup.com>

--- src/helpers/set-commit-status.ts
+++ src/helpers/set-commit-status.ts
@@ -26,7 +26,7 @@ export class SetCommitStatus {
 
 export const setCommitStatus = ({ sha, context, state, description, target_url }: SetCommitStatus) =>
   map(
-    context.split('\n').filter(str => str !== ''),
+    context.split('\n').filter(Boolean),
     async context =>
       octokit.repos.createCommitStatus({
         sha,
