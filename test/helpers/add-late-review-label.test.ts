228587425af0cef2a8e1fbc50a1bb23836540c87
tpoehlman <110557903+tpoehlman@users.noreply.github.com>
2022-08-22 05:36:02 -0700

    feat(add-late-review-label): create add-late-review-label helper (#245)
    * add-pr-stale-label now handles both stale and late review cases, and performed some code cleaning
    
    * Added tests and a null check
    
    * Removed context reference, replaced with direct calls
    
    * Changed to get prs from the api rather than an input
    
    * Added requirement that prs be open
    
    * refactored as to PR comments, a bug exists which prevents the jest mock return value from working in bluebird.map, looking into this
    
    * Refactored to only perform on late label, not stale
    
    * Fixed GH review reccomendations
    
    * Made some changes to fit PR notes, currently tests are not working after putting them inside, looking into this
    
    * Removed get call, transitioned to using requested reviews
    
    * Fixed mocked data
    
    * moved jest.mock return values into the tests, removed the call to 'get'
    
    * implement shared utils method and refactor
    
    * Changed id to number, incorporated context.repo widely, moved mock above imports as will break if done below
    
    * changed id references to number
    
    * added copyright header
    
    * fix test
    
    * rename
    
    * added yml file and updated readme
    
    * Fixed the helper name, changed README to be more abstract
    
    Co-authored-by: Dan Adajian <dadajian@expediagroup.com>

---
+++ test/helpers/add-late-review-label.test.ts
@@ -0,0 +1,106 @@
+/*
+Copyright 2021 Expedia, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+  https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { LATE_REVIEW } from '../../src/constants';
+import { addLateReviewLabel } from '../../src/helpers/add-late-review-label';
+import { context } from '@actions/github';
+import { octokit } from '../../src/octokit';
+import { Mocktokit } from '../types';
+
+jest.mock('@actions/core');
+jest.mock('@actions/github', () => ({
+  context: { repo: { repo: 'repo', owner: 'owner' } },
+  getOctokit: jest.fn(() => ({ rest: { issues: { addLabels: jest.fn() }, pulls: { list: jest.fn() } } }))
+}));
+
+jest.spyOn(Date, 'now').mockImplementation(() => new Date('2022-08-04T10:00:00Z').getTime());
+
+describe('addLateReviewLabel', () => {
+  describe('Late Review', () => {
+    beforeEach(() => {
+      (octokit.pulls.list as unknown as Mocktokit)
+        .mockResolvedValueOnce({
+          status: '200',
+          data: [
+            {
+              number: 123,
+              requested_reviewers: [{ id: 234 }],
+              updated_at: '2022-07-25T20:09:21Z'
+            }
+          ]
+        })
+        .mockResolvedValueOnce({
+          status: '200',
+          data: []
+        });
+    });
+
+    it('should add Late Review label to the pr', async () => {
+      await addLateReviewLabel({
+        days: '1',
+        ...context.repo
+      });
+
+      expect(octokit.pulls.list).toHaveBeenCalledWith({
+        page: 1,
+        per_page: 100,
+        sort: 'updated',
+        direction: 'desc',
+        state: 'open',
+        ...context.repo
+      });
+
+      expect(octokit.pulls.list).toHaveBeenCalledWith({
+        page: 2,
+        per_page: 100,
+        sort: 'updated',
+        direction: 'desc',
+        state: 'open',
+        ...context.repo
+      });
+
+      expect(octokit.issues.addLabels).toHaveBeenCalledWith({
+        labels: [LATE_REVIEW],
+        issue_number: 123,
+        ...context.repo
+      });
+    });
+
+    it('should not add any labels to the pr', async () => {
+      await addLateReviewLabel({
+        days: '1',
+        ...context.repo
+      });
+
+      expect(octokit.pulls.list).toHaveBeenCalledWith({
+        page: 1,
+        per_page: 100,
+        sort: 'updated',
+        direction: 'desc',
+        state: 'open',
+        ...context.repo
+      });
+
+      expect(octokit.pulls.list).toHaveBeenCalledWith({
+        page: 2,
+        per_page: 100,
+        sort: 'updated',
+        direction: 'desc',
+        state: 'open',
+        ...context.repo
+      });
+
+      expect(octokit.issues.addLabels).not.toHaveBeenCalledWith();
+    });
+  });
+});
