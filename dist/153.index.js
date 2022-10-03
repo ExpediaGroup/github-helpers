ec76cb1d9005188da308292406433c75a9288f36
PoojaDonekal1294 <45013586+PoojaDonekal1294@users.noreply.github.com>
2022-10-03 18:18:10 -0500

    fix(assign-pr-reviewers): do nothing if PR is already core approved (#276)
    * remove author from core members
    
    * make it pretty
    
    * Update src/helpers/assign-pr-reviewers.ts
    
    Co-authored-by: Dan Adajian <danadajian@gmail.com>
    
    * Add package-lock.json
    
    * fixups
    
    * update package lock
    
    * Add package-lock.json
    
    * Update src/helpers/assign-pr-reviewers.ts
    
    * update dist files
    
    Co-authored-by: pdonekal <pdonekal@expedia.com>
    Co-authored-by: Dan Adajian <danadajian@gmail.com>
    Co-authored-by: Dan Adajian <dadajian@expediagroup.com>

--- dist/153.index.js
+++ dist/153.index.js
@@ -3,6 +3,73 @@ exports.id = 153;
 exports.ids = [153];
 exports.modules = {
 
+/***/ 9042:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Cc": () => (/* binding */ GITHUB_OPTIONS),
+/* harmony export */   "$9": () => (/* binding */ DEFAULT_PIPELINE_STATUS),
+/* harmony export */   "Km": () => (/* binding */ DEFAULT_PIPELINE_DESCRIPTION),
+/* harmony export */   "Hc": () => (/* binding */ PRODUCTION_ENVIRONMENT),
+/* harmony export */   "fy": () => (/* binding */ LATE_REVIEW),
+/* harmony export */   "_d": () => (/* binding */ CORE_APPROVED_PR_LABEL),
+/* harmony export */   "Xt": () => (/* binding */ PEER_APPROVED_PR_LABEL),
+/* harmony export */   "Ak": () => (/* binding */ READY_FOR_MERGE_PR_LABEL),
+/* harmony export */   "Cb": () => (/* binding */ MERGE_QUEUE_STATUS),
+/* harmony export */   "Ee": () => (/* binding */ QUEUED_FOR_MERGE_PREFIX),
+/* harmony export */   "IH": () => (/* binding */ FIRST_QUEUED_PR_LABEL),
+/* harmony export */   "nJ": () => (/* binding */ JUMP_THE_QUEUE_PR_LABEL),
+/* harmony export */   "HW": () => (/* binding */ DEFAULT_PR_TITLE_REGEX)
+/* harmony export */ });
+/* unused harmony exports DEFAULT_EXEMPT_DESCRIPTION, COPYRIGHT_HEADER */
+/*
+Copyright 2021 Expedia, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+// These extra headers are for experimental API features on Github Enterprise. See https://docs.github.com/en/enterprise-server@3.0/rest/overview/api-previews for details.
+const PREVIEWS = ['ant-man', 'flash', 'groot', 'inertia', 'starfox'];
+const GITHUB_OPTIONS = {
+    headers: {
+        accept: PREVIEWS.map(preview => `application/vnd.github.${preview}-preview+json`).join()
+    }
+};
+const DEFAULT_EXEMPT_DESCRIPTION = 'Passed in case the check is exempt.';
+const DEFAULT_PIPELINE_STATUS = 'Pipeline Status';
+const DEFAULT_PIPELINE_DESCRIPTION = 'Pipeline clear.';
+const PRODUCTION_ENVIRONMENT = 'production';
+const LATE_REVIEW = 'Late Review';
+const CORE_APPROVED_PR_LABEL = 'CORE APPROVED';
+const PEER_APPROVED_PR_LABEL = 'PEER APPROVED';
+const READY_FOR_MERGE_PR_LABEL = 'READY FOR MERGE';
+const MERGE_QUEUE_STATUS = 'QUEUE CHECKER';
+const QUEUED_FOR_MERGE_PREFIX = 'QUEUED FOR MERGE';
+const FIRST_QUEUED_PR_LABEL = `${QUEUED_FOR_MERGE_PREFIX} #1`;
+const JUMP_THE_QUEUE_PR_LABEL = 'JUMP THE QUEUE';
+const DEFAULT_PR_TITLE_REGEX = '^(build|ci|chore|docs|feat|fix|perf|refactor|style|test|revert|Revert|BREAKING CHANGE)((.*))?: .+$';
+const COPYRIGHT_HEADER = (/* unused pure expression or super */ null && (`/*
+Copyright 2021 Expedia, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/`));
+
+
+/***/ }),
+
 /***/ 2153:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
@@ -13,7 +80,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2186);
 /* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _types_generated__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(3476);
+/* harmony import */ var _types_generated__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(3476);
 /* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5438);
 /* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _utils_get_core_member_logins__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(7290);
@@ -23,6 +90,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _octokit__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6161);
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(250);
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var _constants__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(9042);
 /*
 Copyright 2021 Expedia, Inc.
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -52,13 +120,18 @@ var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _argume
 
 
 
-class AssignPrReviewer extends _types_generated__WEBPACK_IMPORTED_MODULE_7__/* .HelperInputs */ .s {
+
+class AssignPrReviewer extends _types_generated__WEBPACK_IMPORTED_MODULE_8__/* .HelperInputs */ .s {
 }
 const assignPrReviewers = ({ teams, login, number_of_assignees = '1', slack_webhook_url, pull_number = String(_actions_github__WEBPACK_IMPORTED_MODULE_1__.context.issue.number) }) => __awaiter(void 0, void 0, void 0, function* () {
     const coreMemberLogins = yield (0,_utils_get_core_member_logins__WEBPACK_IMPORTED_MODULE_2__/* .getCoreMemberLogins */ .c)(_actions_github__WEBPACK_IMPORTED_MODULE_1__.context.issue.number, teams === null || teams === void 0 ? void 0 : teams.split('\n'));
-    const { data: { user } } = yield _octokit__WEBPACK_IMPORTED_MODULE_5__/* .octokit.pulls.get */ .K.pulls.get(Object.assign({ pull_number: _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.issue.number }, _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.repo));
+    const { data: { user, labels } } = yield _octokit__WEBPACK_IMPORTED_MODULE_5__/* .octokit.pulls.get */ .K.pulls.get(Object.assign({ pull_number: _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.issue.number }, _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.repo));
     if (login && coreMemberLogins.includes(login)) {
         _actions_core__WEBPACK_IMPORTED_MODULE_0__.info('Already a core member, no need to assign.');
+        return;
+    }
+    if (labels === null || labels === void 0 ? void 0 : labels.find(label => label.name === _constants__WEBPACK_IMPORTED_MODULE_7__/* .CORE_APPROVED_PR_LABEL */ ._d)) {
+        _actions_core__WEBPACK_IMPORTED_MODULE_0__.info('Already approved by a core member, no need to assign.');
         return;
     }
     const prAuthorUsername = user === null || user === void 0 ? void 0 : user.login;
