cf26c325eab840f76394c417bd5c04a3ff40577e
danadajian <danadajian@gmail.com>
2023-10-04 12:00:43 -0500

    add copyright header

--- dist/325.index.js
+++ dist/325.index.js
@@ -174,6 +174,18 @@ const paginateAllCommentsOnIssue = (issue_number, page = 1) => paginate_comments
 });
 
 ;// CONCATENATED MODULE: ./src/utils/add-due-date-comment.ts
+/*
+Copyright 2023 Expedia, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 var add_due_date_comment_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
