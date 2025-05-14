3f952956514a5acf57d13225e185b886a4cd55f1
Luis Angel Hernández Hernández <jacklu.hdz@gmail.com>
2025-05-14 09:46:44 -0700

    feat(create-pr): Enable branch creation before creating PR (#719)

--- src/helpers/create-pr.ts
+++ src/helpers/create-pr.ts
@@ -15,27 +15,47 @@ import { HelperInputs } from '../types/generated';
 import { context } from '@actions/github';
 import { octokit } from '../octokit';
 import { getDefaultBranch } from '../utils/get-default-branch';
+import simpleGit from 'simple-git';
 
 export class CreatePR extends HelperInputs {
   title = '';
   body = '';
+  declare commit_message?: string;
   declare head?: string;
   declare base?: string;
   declare return_full_payload?: string;
+  declare branch_name?: string;
 }
 
-export const createPr = async ({ title, body, head = context.ref.replace('refs/heads/', ''), base, return_full_payload }: CreatePR) => {
+export const createPr = async ({ title, body, head, base, return_full_payload, branch_name, commit_message }: CreatePR) => {
+  const resolvedHead = await getOrCreateHeadBranch({ head, branch_name, commit_message });
+
   const pr_base = base || (await getDefaultBranch());
-  await updateHeadWithBaseBranch(pr_base, head);
+  await updateHeadWithBaseBranch(pr_base, resolvedHead);
   const { data } = await octokit.pulls.create({
     title,
-    head,
+    head: resolvedHead,
     base: pr_base,
     body,
     maintainer_can_modify: true,
     ...context.repo
   });
   return return_full_payload === 'true' ? data : data.number;
+};
+
+const getOrCreateHeadBranch = async ({ head, branch_name, commit_message }: Partial<CreatePR>): Promise<string> => {
+  if (branch_name && commit_message) {
+    const git = simpleGit();
+
+    await git.checkoutLocalBranch(branch_name);
+    await git.add('.');
+    await git.commit(commit_message);
+    await git.push('origin', branch_name);
+
+    return branch_name;
+  }
+
+  return head || context.ref.replace('refs/heads/', '');
 };
 
 const updateHeadWithBaseBranch = (base: string, head: string) =>
